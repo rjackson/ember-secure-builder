df2d442f0eba823f57e3425ce65630bc413d524a
Robert Jackson <robert.w.jackson@me.com>
2013-09-23 17:15:14 -0400

    Do not load .env data into ENV.

--- lib/ember-secure-builder.rb
+++ lib/ember-secure-builder.rb
@@ -1,8 +1,5 @@
 require 'bundler/setup'
 
-require 'dotenv'
-Dotenv.load
-
 require 'ember-secure-builder/rack_app'
 require 'ember-secure-builder/asset_builder'
 require 'ember-secure-builder/workers/asset_building_worker'
