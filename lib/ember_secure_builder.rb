7abcf71ff68ad422f332a8b651ea7b96bca635f8
Robert Jackson <robert.w.jackson@me.com>
2013-10-02 16:15:10 -0400

    Remove SauceLabsJob (in favor of Webdriver).

--- lib/ember_secure_builder.rb
+++ lib/ember_secure_builder.rb
@@ -2,6 +2,5 @@ require 'bundler/setup'
 
 require_relative 'ember_secure_builder/rack_app'
 require_relative 'ember_secure_builder/asset_builder'
-require_relative 'ember_secure_builder/sauce_labs_job'
 require_relative 'ember_secure_builder/sauce_labs_webdriver_job'
 require_relative 'ember_secure_builder/workers/asset_building_worker'
