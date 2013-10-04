46a619d558d0c63e36f0cb94e9364d9db0c1edb5
Robert Jackson <robert.w.jackson@me.com>
2013-10-04 10:43:37 -0400

    Add mobile test targets.

--- examples/webdriver.rb
+++ examples/webdriver.rb
@@ -4,15 +4,18 @@ Dotenv.load
 
 require_relative '../lib/ember_secure_builder'
 
-BUILD = '90eec26f8c3f930768d9622643dbd8c75f1b7b45'
+BUILD = '2985b2892d0a2c0b75bba7b5e1b5389251b7f1b4'
 URL = "https://s3.amazonaws.com/rwjblue-ember-dev-test/canary/shas/#{BUILD}/tests.html"
 
 RUNS = [{:browser => :chrome,            :platform => 'OS X 10.8'},
-        {:browser => :safari,            :platform => 'OS X 10.8', :version => 6},
-        {:browser => :firefox,           :platform => 'Windows 7', :version => 23},
-        {:browser => :internet_explorer, :platform => 'Windows 7', :version => 10},
-        {:browser => :internet_explorer, :platform => 'Windows 7', :version => 9},
-        {:browser => :internet_explorer, :platform => 'Windows 7', :version => 8},
+        {:browser => :safari,            :platform => 'OS X 10.8',  :version => 6},
+        {:browser => :iphone,            :platform => 'OS X 10.8',  :version => 6,     'device-orientation' => 'landscape'},
+        {:browser => :ipad,              :platform => 'OS X 10.8',  :version => 6,     'device-orientation' => 'landscape'},
+        {:browser => :android,           :platform => 'Linux',      :version => '4.0', 'device-orientation' => 'landscape'},
+        {:browser => :firefox,           :platform => 'Windows 7',  :version => 23},
+        {:browser => :internet_explorer, :platform => 'Windows 7',  :version => 10},
+        {:browser => :internet_explorer, :platform => 'Windows 7',  :version => 9},
+        {:browser => :internet_explorer, :platform => 'Windows 7',  :version => 8},
         {:browser => :internet_explorer, :platform => 'Windows XP', :version => 7},
         {:browser => :internet_explorer, :platform => 'Windows XP', :version => 6}]
 
