0ab38886d1fdb94b202c9c631d093082368e78f7
Robert Jackson <robert.w.jackson@me.com>
2013-10-07 23:21:28 -0400

    Rework example script.

--- examples/webdriver.rb
+++ examples/webdriver.rb
@@ -1,24 +1,15 @@
-require 'pry'
 require 'dotenv'
+require 'octokit'
+require 'rest-client'
+
 Dotenv.load
 
-require_relative '../lib/ember_secure_builder'
-
-BUILD = '2985b2892d0a2c0b75bba7b5e1b5389251b7f1b4'
-URL = "https://s3.amazonaws.com/rwjblue-ember-dev-test/canary/shas/#{BUILD}/ember-tests.html"
-
-def run_all
-  SauceLabsWebdriverJob.default_platforms.each do |hash|
-    EmberSecureBuilder::SauceLabsWorker.perform_async(hash.merge(:url => URL, :build => BUILD, :name => 'Ember Test Run'))
-  end
+pull_requests = Octokit.pull_requests 'emberjs/ember.js'
+pull_requests.each do |pr|
+  RestClient.post 'http://localhost:9292/build', repo: 'emberjs/ember.js', perform_cross_browser_tests: true, pull_request_number: pr.number
 end
 
-def run_one(browser, version)
-  hash = SauceLabsWebdriverJob.all_platforms.find{|h| h[:browser] == browser && h[:version] == version }
-  EmberSecureBuilder::SauceLabsWebdriverJob.new(hash.merge(:url => URL, :build => BUILD, :name => 'Ember Test Run'))
-end
-
-if __FILE__ == $PROGRAM_NAME
-  sauce = run_one(:internet_explorer, 6)
-  binding.pry
+pull_requests = Octokit.pull_requests 'emberjs/data'
+pull_requests.each do |pr|
+  RestClient.post 'http://localhost:9292/build', repo: 'emberjs/data', perform_cross_browser_tests: true, pull_request_number: pr.number
 end
